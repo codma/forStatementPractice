04df5704af923a3e0ef3151c8d9c8594df6cd6d4
Sydney <jinjoolee.0319@gmil.com>
2022-03-11 15:20:01 +0900

    별찍기 수정 - 다이아몬드 만들기 추가

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ for i in range(1,6):
     print("*"*i)
 
 # 역전시키기
-list=[5,4,3,2,1]
-for i in list:
-    print(" "*i,"*"*(6-i))
+for i in range(1, 6):
+    print(' ' * (5 - i) + '*' * i)
 
 #피라미드 만들기
-list=[5,4,3,2,1]
-for i in list:
-    print("  "*i," *"*(6-i),"* "*(6-i))
+for i in range(1, 6):
+    print(' ' * (5 - i) + '*' * (2 * i - 1))
+
+#다이아몬드 만들기
+for i in range(1, 6):
+    print(' ' * (5 - i) + '*' * (2 * i - 1))
+    if 5 == i:
+     for j in range(4, 0, -1):
+        print(' ' * (5 - j) + '*' * (2 * j - 1))
+    for i in range(1, 10):
+        result = ' '
